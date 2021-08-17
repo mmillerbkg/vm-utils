a950451a353f4e6244afccb5e43688df76ad9d79
Michael Miller (Main) <mmiller@bkgllc.com>
2021-08-17 10:55:24 -0500

    Added script for installing docker/docker-compose

---
+++ debian/install-docker.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+# Remove old version if applicable
+apt remove docker docker-engine docker.io containerd runc
+
+apt install lsb-release
+
+wget -O /tmp/docker-keyring.gpg https://download.docker.com/linux/debian/gpg
+
+gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg /tmp/docker-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update && apt install docker-ce docker-ce-cli containerd.io
+
+# Install docker compose
+wget -O /usr/local/bin/docker-compose "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)"
+
+chmod +x /usr/local/bin/docker-compose
