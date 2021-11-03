a557965e2f5bdbe327d12181f5a592f254ed6bfc
Michael Miller (Main) <mmiller@bkgllc.com>
2021-11-03 16:17:21 -0500

    updated docker-compose for v2

--- debian/install-docker.sh
+++ debian/install-docker.sh
@@ -13,9 +13,11 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt update && apt install docker-ce docker-ce-cli containerd.io
+apt update && apt install -y docker-ce docker-ce-cli containerd.io
+
+usermod -aG docker mmiller
 
 # Install docker compose
-wget -O /usr/local/bin/docker-compose "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)"
+wget -O /usr/libexec/docker/cli-plugins/docker-compose "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)"
 
-chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/libexec/docker/cli-plugins/docker-compose
