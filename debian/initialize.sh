3a5b0e51ead9c0fd6676dcc388911e87e98df3d0
Michael Miller (Main) <mmiller@bkgllc.com>
2021-11-03 17:24:12 -0500

    Updated git stuff

--- debian/initialize.sh
+++ debian/initialize.sh
@@ -8,8 +8,7 @@ echo "UFW installed, remember to add rules!"
 
 apt install -y fail2ban && systemctl enable fail2ban
 
-# Change directory to this directory
-cd "${0%/*}"
+git clone https://github.com/mmillerbkg/vm-utils.git && cd vm-utils/debian
 
 cp ./fail2ban.conf /etc/fail2ban/fail2ban.local && cp ./jail.conf /etc/fail2ban/jail.local
 
@@ -33,7 +32,7 @@ timedatectl set-timezone US/Central
 # Change to root dir
 cd ~
 ## Install build tools
-apt install -y make cmake gcc g++ flex tar bison
+apt install -y make cmake gcc flex tar bison
 git clone https://salsa.debian.org/debian/doas.git
 
 cd ~/doas
